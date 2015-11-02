26c632225e11660610a378d99ae66d05280078f2
Emily Marriott <emilycoralmarriott@hotmail.co.uk>
2015-11-02 15:26:48 +0000

    Added tags to ball prefab and added ball clean up trigger and script

---
+++ New Unity Project/Assets/BallCleanUp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallCleanUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag.Equals("Ball"))
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
