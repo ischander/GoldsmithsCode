4170db055cef2ab5655c5c47c81342998a4cc8ae
Emily Marriott <emilycoralmarriott@hotmail.co.uk>
2015-11-02 15:14:19 +0000

    Added ball spawners

--- New Unity Project/Assets/Scripts/BallSpawner.cs
+++ New Unity Project/Assets/Scripts/BallSpawner.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class BallSpawner : MonoBehaviour {
 
+    public GameObject ball;
+    bool spawnBalls = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,27 @@ public class BallSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spawnBalls = !spawnBalls;
+            StartCoroutine(Spawn());
+        }
 	}
+
+    IEnumerator Spawn()
+    {
+        while (spawnBalls)
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        yield return null;
+    }
 }
