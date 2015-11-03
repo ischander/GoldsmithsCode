69e2189a4e89f5b735dd76b7cdc6a05c3a028458
Emily Marriott <emilycoralmarriott@hotmail.co.uk>
2015-11-03 12:17:16 +0000

    Added rooms in a vector and changed the print outs to print out text for the current room (int position in the vector). Added error checking for options.

--- CPPModule/CPPModule/CPPModule.cpp
+++ CPPModule/CPPModule/CPPModule.cpp
@@ -5,32 +5,50 @@
 #include "stdafx.h"
 //copies content of iostream into file
 #include <iostream>
+#include<vector>
+#include<string>
+using std::vector;
+using std::string;
 
 //command line parameters - arguments to be passed to the program when ran in command line
 int main(int argc, const char * argv[])
 {
 	int stopper;
 	int selectedOption = 0;
+	int currentRoom = 0;
+
+	vector<std::string> rooms;
+	rooms.push_back("You are standing on the edge of a deep dark forest.");
+	rooms.push_back("You are standing in the forest, its dark in here");
+	rooms.push_back("You see a clearing");
 
 	//std:: - namespace (iostream library stops confusion with same named functions with other libraries)
 	//<< - operator, capable of being overloaded, can create custom versions for each class
 	//<< - send to thing on the left
 	
 	while (true) {
-		std::cout << "You are standing on the edge of a deep dark forest." << std::endl;
+		std::cout << rooms[currentRoom] << std::endl;
 		std::cout << "1. Go In" << std::endl;
 		std::cout << "2. Stay outside" << std::endl;
+		std::cout << "3. Go Back" << std::endl;
 
 		//This is true or false - used for validation
 		if (std::cin >> selectedOption) {
 			if (selectedOption == 1) {
-				std::cout << "You are standing in the forest, its dark in here" << std::endl;
+				if (currentRoom < rooms.size() - 1) {
+					currentRoom+=1;
+				}
 			}
 			else if (selectedOption == 2) {
-				std::cout << "You are standing on the edge of a deep dark forest. Not much of an adventure!" << std::endl;
+				
+			}
+			else if (selectedOption == 3) {
+				if (currentRoom > 0) {
+					currentRoom-= 1;
+				}
 			}
 			else {
-				std::cout << "Please type 1 or 2" << std::endl;
+				std::cout << "Please type 1 or 2 or 3" << std::endl;
 			}
 		}
 		else
